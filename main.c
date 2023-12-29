68c2c4546f581b2c1417a61095a4a3b6b0da80de
alkuzin <alkuzindev@gmail.com>
2023-12-29 19:36:42 +0300

    Updated main.c file

---
+++ main.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                                            */
+/*   main.c                                                                   */
+/*                                                                            */
+/*   By: alkuzin <->                                                          */
+/*                                                                            */
+/*   Created: 2023/12/29 12:00:17 by alkuzin                                  */
+/*   Updated: 2023/12/29 19:24:35 by alkuzin                                  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "include/hexd.h"
+
+static int set_base(char chr_base);
+
+
+int main(int argc, char** argv)
+{
+	FILE *fp;
+    int base;
+    char filename[FILENAME_SIZE];
+
+	if (argc < 2 || argc > 3)
+    {
+        puts("[!] Incorrect number of arguments\n"
+        "Use '--help' or '-h' to see options list");
+        exit(1);
+    }
+
+    if ((strncmp(argv[1], "--help", 6) == 0) || (strncmp(argv[1], "-h", 2) == 0))
+        show_help();
+
+    if ((strncmp(argv[1], "--version", 9) == 0) || (strncmp(argv[1], "-v", 2) == 0))
+        show_version();
+
+    if (!(argv[1][0] == '-') || strlen(argv[1]) <= 1)
+    {
+        printf("[!] Incorrect option '%s'\n"
+        "Use '--help' or '-h' to see options list\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+    
+    strncpy(filename, argv[2], FILENAME_SIZE);
+    if(!(fp = fopen(filename, "rb")))
+    {
+        printf("[x] Error to open file '%s'\n"
+        "File doesn't exist or corrupted\n", filename);
+        exit(EXIT_FAILURE);
+	}	
+    
+	if ((base = set_base(argv[1][1])) == -1)
+	{
+        printf("[!] Incorrect option '%s'\n"
+        "Use '--help' or '-h' to see options list\n", argv[1]);
+        exit(EXIT_FAILURE);
+	}
+
+    show_bytes(fp, base);
+    
+    if (!feof(fp))
+    {
+        fpos_t pos;
+        fgetpos(fp, &pos);
+        printf("[x] Can't reach the end of the file on pos: %lld\n", (long long int)pos.__pos);
+    }
+
+    fclose(fp);
+    return 0;
+}
+
+static int set_base(char chr_base)
+{
+	int base;
+
+
+    switch (chr_base)
+    {
+    case 'x':
+        base = HEX;
+        break;
+    
+    case 'o':
+        base = OCT;
+        break;
+    
+    case 'b':
+        base = BIN;
+        break;
+
+    default:
+		base = -1;
+        break;
+    }
+
+	return base;
+}
